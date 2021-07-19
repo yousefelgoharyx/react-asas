62a8f3212f6115dc609c72e0e707d9b01a712466
Yousef M. El-Gohary <yousefelgoharyx@gmail.com>
2021-07-19 18:18:12 +0200

    v0.0.1 complete

--- example/src/App.js
+++ example/src/App.js
@@ -1,49 +1,28 @@
 import React from 'react'
-
-import { VStack, Image, Text, View, Scroll, AppLayout, HStack } from 'asas'
-
+import {AppLayout, View, VStack, Scroll} from 'react-ui'
 const App = () => {
     return (
         <AppLayout>
-            <View style={{backgroundColor: "#111", height: 56, color: "#fff"}}>Bar</View>
+            <View style={{backgroundColor: "#111"}}>
+                Navbar
+            </View>
             <Scroll>
-                <VStack gap={16}>
-                    <HStack gap={16}>
-
-                    <View style={{ backgroundColor: '#f33' }}>Hello boy</View>
-                     <View style={{ backgroundColor: '#f33' }}>Hello boy</View>
-                     <View style={{ backgroundColor: '#f33' }}>Hello boy</View>
-                    </HStack>
-                     <View style={{ backgroundColor: '#f33' }}>Hello boy</View>
-                     <View style={{ backgroundColor: '#f33' }}>Hello boy</View>
-                     <View style={{ backgroundColor: '#f33' }}>Hello boy</View>
-                     <View style={{ backgroundColor: '#f33' }}>Hello boy</View>
-                     <View style={{ backgroundColor: '#f33' }}>Hello boy</View>
-                     <View style={{ backgroundColor: '#f33' }}>Hello boy</View>
-                     <View style={{ backgroundColor: '#f33' }}>Hello boy</View>
-                     <View style={{ backgroundColor: '#f33' }}>Hello boy</View>
-                     <View style={{ backgroundColor: '#f33' }}>Hello boy</View>
-                     <View style={{ backgroundColor: '#f33' }}>Hello boy</View>
-                     <View style={{ backgroundColor: '#f33' }}>Hello boy</View>
-                     <View style={{ backgroundColor: '#f33' }}>Hello boy</View>
-                     <View style={{ backgroundColor: '#f33' }}>Hello boy</View>
-                     <View style={{ backgroundColor: '#f33' }}>Hello boy</View>
-                     <View style={{ backgroundColor: '#f33' }}>Hello boy</View>
-                     <View style={{ backgroundColor: '#f33' }}>Hello boy</View>
-                     <View style={{ backgroundColor: '#f33' }}>Hello boy</View>
-                     <View style={{ backgroundColor: '#f33' }}>Hello boy</View>
-                     <View style={{ backgroundColor: '#f33' }}>Hello boy</View>
-                     <View style={{ backgroundColor: '#f33' }}>Hello boy</View>
-                     <View style={{ backgroundColor: '#f33' }}>Hello boy</View>
-                     <View style={{ backgroundColor: '#f33' }}>Hello boy</View>
-                     <View style={{ backgroundColor: '#f33' }}>Hello boy</View>
-                     <View style={{ backgroundColor: '#f33' }}>Hello boy</View>
-                     <View style={{ backgroundColor: '#f33' }}>Hello boy</View>
-                     <View style={{ backgroundColor: '#f33' }}>Hello boy</View>
-                     <View style={{ backgroundColor: '#f33' }}>Hello boy</View>
+                <VStack gap={16} padding={32}>
+                    <View style={{backgroundColor: "#222", height: 200}}>Hello boy</View>
+                    <View style={{backgroundColor: "#222", height: 200}}>Hello boy</View>
+                    <View style={{backgroundColor: "#222", height: 200}}>Hello boy</View>
+                    <View style={{backgroundColor: "#222", height: 200}}>Hello boy</View>
+                    <View style={{backgroundColor: "#222", height: 200}}>Hello boy</View>
+                    <View style={{backgroundColor: "#222", height: 200}}>Hello boy</View>
+                    <View style={{backgroundColor: "#222", height: 200}}>Hello boy</View>
+                    <View style={{backgroundColor: "#222", height: 200}}>Hello boy</View>
+                    <View style={{backgroundColor: "#222", height: 200}}>Hello boy</View>
+                    <View style={{backgroundColor: "#222", height: 200}}>Hello boy</View>
                 </VStack>
             </Scroll>
-            <View style={{backgroundColor: "#111", height: 56, color: "#fff"}}>Bar</View>
+            <View style={{backgroundColor: "#444"}}>
+                Bottom Navigation
+            </View>
         </AppLayout>
     )
 }
